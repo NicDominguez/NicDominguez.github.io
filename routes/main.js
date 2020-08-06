57a0def41da3e7b4a1a4ce42ad84895d5202a513
NicDominguez <NicDominguez@users.noreply.github.com>
2020-08-06 16:58:54 -0700

    creates technology list for filtering on home page(no functionality)

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json')
 
+const technologiesArray = projects.map(project => project.technologies)
+
+function flatten(arr) {
+    let flat = []
+    for (var i = 0; i< arr.length; i++) {
+        flat = flat.concat(arr[i])
+    }
+    return flat;
+}
+
+const allTechnologies = flatten(technologiesArray)
+const techs = allTechnologies.filter((tech, i) => {
+    return allTechnologies.indexOf(tech) == i
+})
+
 // Renders index pug file on root route
 router.get('/', (req, res) => {
-    res.render('index', { projects });
+    res.render('index', { projects, techs });
 });
 
 // Renders about pug file on about route
